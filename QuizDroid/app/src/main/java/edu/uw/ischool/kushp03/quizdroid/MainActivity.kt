c1dd630f30ba0dfe617cabeb51b0ede444eaa76a
KushPData <121984432+KushPData@users.noreply.github.com>
2024-11-13 19:00:52 -0800

    Added network detection/network-status-based messages and data downloading. Still having problems with changing the downloaded files once the user preference changes

--- QuizDroid/app/src/main/java/edu/uw/ischool/kushp03/quizdroid/MainActivity.kt
+++ QuizDroid/app/src/main/java/edu/uw/ischool/kushp03/quizdroid/MainActivity.kt
@@ -1,15 +1,24 @@
 package edu.uw.ischool.kushp03.quizdroid
 
+import android.app.Application
+import android.app.PendingIntent.getActivity
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.provider.Settings
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var goToPreferences: Button
+    private lateinit var topicRepository: TopicRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +30,15 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        topicRepository = (application as QuizApp).getTopicRepository()
+
         getTopicListFragment()
+
+        if (deviceIsConnected()) {
+            downloadData()
+        } else {
+            askForUserInputRelatedToNetwork()
+        }
 
         goToPreferences = findViewById(R.id.goToPreferences)
 
@@ -29,6 +46,67 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, Preferences::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun deviceIsConnected(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+
+    private fun downloadData() {
+        val sharedPreference = getSharedPreferences("userPreferences", MODE_PRIVATE)
+        val url = sharedPreference.getString("newUrl", "http://tednewardsandbox.site44.com/questions.json")
+        (topicRepository as TopicRepositoryImplementation).apply {
+            setDownLoadStatusListener(object : TopicRepositoryImplementation.DownloadStatusListener {
+                override fun downloadStatus(wasSuccessful: Boolean, message: String) {
+                    runOnUiThread {
+                        if(wasSuccessful) {
+                            Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+                        } else {
+                            askForUserInputRelatedToFailedDownload(message)
+
+                        }
+                    }
+                }
+            })
+            fetch(url)
+        }
+    }
+
+    private fun askForUserInputRelatedToFailedDownload(message: String) {
+        val downloadNotification = AlertDialog.Builder(this)
+
+        downloadNotification.setTitle("Could not download data")
+        downloadNotification.setMessage("$message. Do you want to try downloading again?")
+            .setPositiveButton("Retry") { _, _ -> downloadData()}
+            .setNegativeButton("Quit App") { _, _ -> finish()}
+    }
+
+    private fun askForUserInputRelatedToNetwork() {
+        val networkNotification = AlertDialog.Builder(this)
+
+        networkNotification.setTitle("Could not connect to the internet")
+
+        val checkIfAirplaneModeOn = Settings.Global.getInt(contentResolver, Settings.Global.AIRPLANE_MODE_ON, 0) != 0
+
+        if(checkIfAirplaneModeOn) {
+            networkNotification.setMessage("Airplane mode detected. Do you want to turn it off?")
+                .setPositiveButton("Settings") { _, _ -> startActivity(Intent(Settings.ACTION_AIRPLANE_MODE_SETTINGS))}
+                .setNegativeButton("Close") {dialog, _ -> dialog.dismiss()}
+        } else {
+            networkNotification.setMessage("No signal detected")
+                .setPositiveButton("Retry") {_, _ ->
+                    if(deviceIsConnected()) {
+                        downloadData()
+                    } else {
+                        askForUserInputRelatedToNetwork()
+                    }
+                }
+                .setNegativeButton("Quit") {_, _ -> finish()}
+        }
+        networkNotification.show()
     }
 
     private fun getTopicListFragment() {
--- QuizDroid/app/src/main/java/edu/uw/ischool/kushp03/quizdroid/TopicRepositoryImplementation.kt
+++ QuizDroid/app/src/main/java/edu/uw/ischool/kushp03/quizdroid/TopicRepositoryImplementation.kt
@@ -1,34 +1,89 @@
 package edu.uw.ischool.kushp03.quizdroid
 
 import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity.MODE_PRIVATE
 import org.json.JSONArray
-import org.json.JSONObject
+import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.io.IOException
+import java.net.HttpURLConnection
+import java.util.concurrent.Executors
+import java.net.URL
 
 class TopicRepositoryImplementation(private val context: Context) : TopicRepository {
-    private val hardcodedTopics = arrayListOf<TopicModel>(
-        TopicModel("Science!", "Because SCIENCE!", "Because SCIENCE!", listOf<QuestionModel>(
-            QuestionModel("What is fire?", listOf<String>("One of the four classical elements", "A magical reaction given to us by God", "A band that hasn't yet been discovered", "Fire! Fire! Fire! heh-heh"), 1)
-        )),
-        TopicModel("Marvel Super Heroes", "Avengers, Assemble!", "Avengers, Assemble!", listOf<QuestionModel>(
-            QuestionModel("Who is Iron Man?", listOf<String>("Tony Stark", "Obadiah Stane", "A rock hit by Megadeth", "Nobody knows"), 1),
-            QuestionModel("Who founded the X-men?", listOf<String>("Tony Stark", "Professor X", "The X-Institute", "Erik Lensherr"), 2),
-            QuestionModel("How did Spider-Man get his powers?", listOf<String>("He was bitten by a radioactive spider", "He ate a radioactive spider", "He is a radioactive spider", "He looked at a radioactive spider"), 1)
-        )),
-        TopicModel("Mathematics", "Did you pass the third grade?", "Did you pass the third grade?", listOf<QuestionModel>(
-            QuestionModel("What is 2+2?", listOf<String>("4", "22", "An irrational number", "Nobody knows"), 1)
-        ))
-    )
+//    private val hardcodedTopics = arrayListOf<TopicModel>(
+//        TopicModel("Science!", "Because SCIENCE!", "Because SCIENCE!", listOf<QuestionModel>(
+//            QuestionModel("What is fire?", listOf<String>("One of the four classical elements", "A magical reaction given to us by God", "A band that hasn't yet been discovered", "Fire! Fire! Fire! heh-heh"), 1)
+//        )),
+//        TopicModel("Marvel Super Heroes", "Avengers, Assemble!", "Avengers, Assemble!", listOf<QuestionModel>(
+//            QuestionModel("Who is Iron Man?", listOf<String>("Tony Stark", "Obadiah Stane", "A rock hit by Megadeth", "Nobody knows"), 1),
+//            QuestionModel("Who founded the X-men?", listOf<String>("Tony Stark", "Professor X", "The X-Institute", "Erik Lensherr"), 2),
+//            QuestionModel("How did Spider-Man get his powers?", listOf<String>("He was bitten by a radioactive spider", "He ate a radioactive spider", "He is a radioactive spider", "He looked at a radioactive spider"), 1)
+//        )),
+//        TopicModel("Mathematics", "Did you pass the third grade?", "Did you pass the third grade?", listOf<QuestionModel>(
+//            QuestionModel("What is 2+2?", listOf<String>("4", "22", "An irrational number", "Nobody knows"), 1)
+//        ))
+//    )
 
+    private val executor = Executors.newFixedThreadPool(5)
+    private var downloadStatusListener: DownloadStatusListener? = null
+
+    interface DownloadStatusListener {
+        fun downloadStatus(wasSuccessful: Boolean, message: String)
+    }
+
+    fun setDownLoadStatusListener(listener: DownloadStatusListener) {
+        downloadStatusListener = listener
+    }
+
+    fun fetch(url: String?) {
+//        val url = "http://tednewardsandbox.site44.com/questions.json"
+
+        Toast.makeText(context, "Downloading from $url", Toast.LENGTH_SHORT).show()
+
+        executor.execute {
+            try {
+                val urlConnection = URL(url).openConnection() as HttpURLConnection
+                val responseStream = ByteArrayOutputStream()
+
+                try {
+                    val incoming = BufferedInputStream(urlConnection.inputStream).bufferedReader()
+                    Log.i("TopicRepositoryImplementation", "got the incoming buffered stream")
+                    incoming.forEachLine {
+                        Log.i("TopicRepositoryImplementation", "it.toByteArray()${it.toByteArray()}")
+                        responseStream.write(it.toByteArray())
+                    }
+                } finally {
+                    urlConnection.disconnect()
+                }
+
+                val data = responseStream.toString()
+
+                if (data.isNotEmpty()) {
+                    val fileOutputStream: FileOutputStream = context.openFileOutput("questions.json", Context.MODE_PRIVATE)
+                    val outputStreamWriter = OutputStreamWriter(fileOutputStream)
+                    outputStreamWriter.write(data)
+                    outputStreamWriter.close()
+
+                    downloadStatusListener?.downloadStatus(true, "Download Successful")
+                } else {
+                    downloadStatusListener?.downloadStatus(false, "Empty Response")
+                }
+            }
+            catch (e: IOException) {
+                downloadStatusListener?.downloadStatus(false, "Download Failed with error: ${e.message}")
+            }
+
+        }
+    }
 
     override fun getTopics() : ArrayList<TopicModel> {
-
-//        Making a json file using hardcodedTopics
-        addTopicsToFile(hardcodedTopics)
 
         val topics = arrayListOf<TopicModel>()
 
@@ -40,6 +95,7 @@ class TopicRepositoryImplementation(private val context: Context) : TopicReposit
             for(i in 0 until jsonArray.length()) {
                 val topicObject = jsonArray.getJSONObject(i)
                 val title = topicObject.getString("title")
+                Toast.makeText(context, "Title: $title", Toast.LENGTH_SHORT).show()
                 val desc = topicObject.getString("desc")
                 val questionsArray = topicObject.getJSONArray("questions")
 
@@ -73,46 +129,46 @@ class TopicRepositoryImplementation(private val context: Context) : TopicReposit
         return topics
     }
 
-    private fun addTopicsToFile(topics: List<TopicModel>) {
-        try {
-            val jsonArray = JSONArray()
-
-            for (topic in topics) {
-                val topicObject = JSONObject()
-                topicObject.put("title", topic.topicName)
-//                Once again, using short description and long description with the same value
-                topicObject.put("desc", topic.topicShortDescription)
-
-                val questionsArray = JSONArray()
-
-                for(question in topic.questions) {
-                    val questionObject = JSONObject()
-
-                    questionObject.put("text", question.question)
-                    questionObject.put("answer", question.correctAnswer.toString())
-
-                    val answersArray = JSONArray()
-
-                    for(answer in question.answerChoices) {
-                        answersArray.put(answer)
-                    }
-
-                    questionObject.put("answers", answersArray)
-
-                    questionsArray.put(questionObject)
-                }
-
-                topicObject.put("questions", questionsArray)
-                jsonArray.put(topicObject)
-            }
-
-            val fileOutputStream: FileOutputStream = context.openFileOutput("questions.json", Context.MODE_PRIVATE)
-            val outputStreamWriter = OutputStreamWriter(fileOutputStream)
-            outputStreamWriter.write(jsonArray.toString())
-            outputStreamWriter.close()
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
+//    private fun addTopicsToFile(topics: List<TopicModel>) {
+//        try {
+//            val jsonArray = JSONArray()
+//
+//            for (topic in topics) {
+//                val topicObject = JSONObject()
+//                topicObject.put("title", topic.topicName)
+////                Once again, using short description and long description with the same value
+//                topicObject.put("desc", topic.topicShortDescription)
+//
+//                val questionsArray = JSONArray()
+//
+//                for(question in topic.questions) {
+//                    val questionObject = JSONObject()
+//
+//                    questionObject.put("text", question.question)
+//                    questionObject.put("answer", question.correctAnswer.toString())
+//
+//                    val answersArray = JSONArray()
+//
+//                    for(answer in question.answerChoices) {
+//                        answersArray.put(answer)
+//                    }
+//
+//                    questionObject.put("answers", answersArray)
+//
+//                    questionsArray.put(questionObject)
+//                }
+//
+//                topicObject.put("questions", questionsArray)
+//                jsonArray.put(topicObject)
+//            }
+//
+//            val fileOutputStream: FileOutputStream = context.openFileOutput("questions.json", Context.MODE_PRIVATE)
+//            val outputStreamWriter = OutputStreamWriter(fileOutputStream)
+//            outputStreamWriter.write(jsonArray.toString())
+//            outputStreamWriter.close()
+//
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
+//    }
 }
